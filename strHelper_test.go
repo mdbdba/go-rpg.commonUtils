c73eaa7a8dad48b3f325d72dd8d3cfa52d384edc
mdbdba <mdbntl@gmail.com>
2022-01-16 11:20:51 -0600

    changes for v0.4.0
    Add a random string generator

--- strHelper_test.go
+++ strHelper_test.go
@@ -16,3 +16,9 @@ func TestStringSliceToString(t *testing.T) {
 	expected := "[\"test\", \"all\", \"things\"]"
 	assert.Equal(t, expected, actual)
 }
+
+func TestGenerateRandomString(t *testing.T) {
+	actual, err := GenerateRandomString(5)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 5, len(actual))
+}
